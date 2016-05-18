94eb8c8bd71a898b23d280c4eb1bc571057aefe4
DreamLCM <mac@mac.local>
2016-05-18 14:59:34 +0800

    Initial Commit

---
+++ QuartzFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuartzFun
+//
+//  Created by MAC on 16/5/18.
+//  Copyright © 2016年 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
